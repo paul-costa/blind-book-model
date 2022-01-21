ac1326c2b22f79d5706544d6287440d096b58f50
Paul Costa <admin@costapaul.com>
2022-01-21 22:48:03 +0100

    init commit

---
+++ src/app/Models/BlindBookModel.model.ts
@@ -0,0 +1,96 @@
+
+export enum GenreNames {
+  'Fantasy',
+  'Romance',
+  'Thriller',
+  'young adult',
+  'non-fiction'
+}
+
+
+export enum Trigger {
+  'sh/su',
+  'drugs',
+  'rape'
+}
+
+export enum Mood {
+  'dark',
+  'light'
+}
+
+
+export enum nomScale {
+  'low',
+  'middle',
+  'high'
+}
+
+export enum Series {
+  false,
+  'standalone',
+  'series',
+}
+
+
+// first stage
+export class BlindBookModel {
+  dimensions: {
+    heightWidth: nomScale;
+    backType: 'paper' | 'hard';
+  };
+
+  character: {
+    genreName: GenreNames[];
+    trigger: undefined | Trigger[];
+    mood: Mood[];
+    paced: nomScale;
+    series: Series;
+    adaption: undefined | false | 'book-to-new-media' | 'new-media-to-book';
+  };
+
+  additionalInformation: {
+    text: string;           // min 280 zeichen
+  };
+}
+
+
+export enum PrecedingKnowledgeNeeded {
+  'religion',
+  'history',
+  'sociology',
+  'philosophy',
+  'psychology',
+  'stem',
+  'politics',
+  'economy',
+  'law',
+  'technology',
+}
+
+export enum Theme {
+  'revenge',
+  'love',
+  'growth',
+  'balance',
+  'unity',
+  'anarchy',
+  'misc'
+}
+
+
+// second stage
+export class WokeBookModel extends BlindBookModel {
+  override dimensions: {
+    heightWidth: nomScale;
+    backType: 'paper' | 'hard';
+    length: nomScale;
+  };
+
+  override additionalInformation: {
+    text: string;           // min 280 zeichen
+    price: undefined | nomScale;
+    precedingKnowledgeNeeded: undefined | PrecedingKnowledgeNeeded[];
+    motive: Theme[];
+  };
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlindDateBookModelComponent } from './Models/blind-date-book-model/blind-date-book-model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlindDateBookModelComponent
   ],
   imports: [
     BrowserModule,
